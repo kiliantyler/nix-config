9bd38cd64b38089dc0b29c3d23d47b3fd9c224b3
Kilian Tyler <github@ktyler.me>
2024-01-08 21:00:49 -0500

    reload shell

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+{ # Prevents execution if this script was only partially downloaded
 
 git clone https://github.com/kiliantyler/nix-config ~/.nix-config
 
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.nix-config/.task
 
 ~/.nix-config/.task/task -t ~/.nix-config/Taskfile.yaml nix:install
+
+exec $SHELL
+
+}
