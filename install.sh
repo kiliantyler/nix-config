450eecc309abf60ea7aa614947e5e2a07e0640a5
Kilian Tyler <github@ktyler.me>
2024-01-13 13:20:16 -0500

    fix: Taskfiles don't let you override the path... for some reason

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ git clone https://github.com/kiliantyler/nix-config ~/nix-config
 
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/nix-config/.task
 
-~/nix-config/.task/task -t ~/nix-config/Taskfile.yaml nix:setup-all
+~/nix-config/.task/task -d ~/nix-config nix:setup-all
 
 }
