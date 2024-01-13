28aa1ed612fbab3b419314fcc7b31d63e425d1a4
Kilian Tyler <github@ktyler.me>
2024-01-13 13:59:03 -0500

    fix: move path issues to install.sh since we only need them on first run

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 { # Prevents execution if this script was only partially downloaded
 
-git clone https://github.com/kiliantyler/nix-config ~/nix-config
+PATH="/fakepath:$PATH"
+# git clone https://github.com/kiliantyler/nix-config ~/nix-config
 
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/nix-config/.task
+# sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/nix-config/.task
 
-~/nix-config/.task/task -d ~/nix-config nix:setup-all
+# ~/nix-config/.task/task -d ~/nix-config nix:setup-all
+
+task path
 
 }
