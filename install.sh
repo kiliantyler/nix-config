f532f2e223af09aef63162a7a7de4265bcba43c9
Kilian Tyler <github@ktyler.me>
2024-01-13 14:01:56 -0500

    fix: fix fix fix fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 { # Prevents execution if this script was only partially downloaded
 
 # Allows us to run this as if everything were already installed
-PATH="${HOME}/nix-config/.task:/nix/var/nix/profiles/default/bin"
+PATH="${HOME}/nix-config/.task:/nix/var/nix/profiles/default/bin:$PATH"
 
 git clone https://github.com/kiliantyler/nix-config ~/nix-config
 
