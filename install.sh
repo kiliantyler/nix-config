192590e5b5bc82f03de6deb6dad92125055e6b72
Kilian Tyler <github@ktyler.me>
2024-01-13 12:44:08 -0500

    fix: change task install dir

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 
 git clone https://github.com/kiliantyler/nix-config ~/nix-config
 
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.nix-config/.task
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/nix-config/.task
 
-~/.nix-config/.task/task -t ~/nix-config/Taskfile.yaml nix:install
+~/nix-config/.task/task -t ~/nix-config/Taskfile.yaml nix:install
 
 }
