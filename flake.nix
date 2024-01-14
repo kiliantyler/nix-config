17ac61cc840a685033cf56364d805e787bd4b542
Kilian Tyler <github@ktyler.me>
2024-01-13 21:56:13 -0500

    feat: attempt 1

--- flake.nix
+++ flake.nix
@@ -1,61 +1,20 @@
 {
-  description = "kiliantyler Nix Flake";
+  description = "Darwin attempt 1";
 
   inputs = {
-    # Nixpkgs and unstable
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    # Home manager
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # nix-darwin
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Flake-parts
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { flake-parts, ... }@inputs:
-    let
-      myLib = import ./lib/default.nix { inherit inputs; };
-    in
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
-      perSystem = {
-        inputs',
-        pkgs,
-        self',
-        ...
-      }: {
-        legacyPackages = import ./packages { inherit inputs' pkgs; };
-        packages = {
-          harlequin = self'.legacyPackages.harlequin;
+  outputs = { self, nix-darwin, nixpkgs, ... }: {
+    darwinConfigurations = {
+      mac = darwin.lib.darwinSystem
+        {
+          modules = [
+            ./darwin-configuration.nix
+          ];
+        }
         };
-        # devShells.default = import ./nixos/packages/shell.nix { inherit inputs' pkgs; };
-      };
-
-      flake.nixosConfigurations = {
-        nas = myLib.mkNixosSystem     "x86_64-linux"  "gladius";
-        test-vm = myLib.mkNixosSystem "aarch64-linux" "test-vm";
-      };
-
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#<hostname>
-      flake.darwinConfigurations = {
-        personal-macbook = myLib.mkDarwinSystem "aarch64-darwin" "bernd-macbook";
-      };
     };
-}
+  }
