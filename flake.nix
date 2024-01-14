09604a7dba3fae08b48a62afb27c5e77c224073f
Kilian Tyler <github@ktyler.me>
2024-01-13 22:48:36 -0500

    fix: hm url

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs = { self, nix-darwin, nixpkgs, ... }: {
